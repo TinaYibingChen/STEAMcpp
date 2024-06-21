76011a3804f3a0b953acb44863dd0129260fead5
sydneyohr <sydney.ohr@gmail.com>
2024-06-21 11:29:01 -0500

    Update bioconductor_dependencies.R

--- R/bioconductor_dependencies.R
+++ R/bioconductor_dependencies.R
@@ -6,14 +6,9 @@
  .onLoad <- function() {
   if (!requireNamespace("BiocManager", quietly = TRUE)) {
     install.packages("BiocManager")
-  }
   BiocManager::install(c("gdsfmt", "SeqArray", "SNPRelate"))
   
 }
-
-
-
-
 
   
 }

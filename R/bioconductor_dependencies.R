528b5e1b1aadb0abbd142c017bb6da1416eb38bb
sydneyohr <sydney.ohr@gmail.com>
2024-06-24 09:27:24 -0500

    deleted parallelize simstat

--- R/bioconductor_dependencies.R
+++ R/bioconductor_dependencies.R
@@ -1,12 +1,10 @@
 
-#' @import BiocManager
-#' @import SNPRelate
-#' @import SeqArray
-#' @import gdsfmt
+
  .onLoad <- function() {
   if (!requireNamespace("BiocManager", quietly = TRUE)) {
     install.packages("BiocManager")
-  BiocManager::install(c("gdsfmt", "SeqArray", "SNPRelate"))
+  
+    BiocManager::install(c("gdsfmt", "SeqArray", "SNPRelate"))
   
 }
 
